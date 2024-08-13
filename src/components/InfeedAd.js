15f005ff645c9e35fb811f0b6f0d0ecac774b6dc
OmkarMundlik <omkarmundlik11@gmail.com>
2024-08-13 19:36:28 +0530

    Updated Infeed ad

--- src/components/InfeedAd.js
+++ src/components/InfeedAd.js
@@ -1,29 +1,32 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 
-const AdSenseComponent = () => {
-  const adRef = useRef(null);
-
+export default function InfeedAd() {
   useEffect(() => {
-    if (adRef.current) {
+    const loadAd = () => {
       try {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
+        if (window.adsbygoogle && window.adsbygoogle.loaded) {
+          window.adsbygoogle.push({});
+        }
       } catch (e) {
         console.error('AdSense error', e);
       }
-    }
+    };
+
+    const adTimeout = setTimeout(() => {
+      loadAd();
+    }, 1000); // Delay to ensure DOM is ready
+
+    return () => {
+      clearTimeout(adTimeout);
+    };
   }, []);
 
   return (
-    <ins
-      ref={adRef}
-      className="adsbygoogle"
+    <ins className="adsbygoogle"
       style={{ display: 'block' }}
       data-ad-format="fluid"
       data-ad-layout-key="-6t+ed+2i-1n-4w"
       data-ad-client="ca-pub-5947514247098170"
-      data-ad-slot="8014478239"
-    ></ins>
+      data-ad-slot="8014478239"></ins>
   );
-};
-
-export default AdSenseComponent;
+}
